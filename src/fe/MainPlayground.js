a2b308d61043415153ab80066db47a141480af0e
Matyáš Neuvirt <matyas.neuvirt@gmail.com>
2022-04-26 00:44:59 +0200

    finished rewrite of basic object changes tracking

--- src/fe/MainPlayground.js
+++ src/fe/MainPlayground.js
@@ -1,5 +1,6 @@
 import {EntityContainer} from "@Lib/shared/containers/EntityContainer";
 import {pack} from 'msgpackr'
+import {clear} from "@Lib/objUtil";
 
 const proxyE = document.getElementById('proxy');
 const simE = document.getElementById('sim');
@@ -7,35 +8,48 @@ const changesE = document.getElementById('changes');
 const clearE = document.getElementById('clear');
 
 const c = new EntityContainer()
-c.proxy.x = 50
-c.proxy.x
-delete c.proxy.x
+c.proxy.a = 40
+c.proxy.b = {x: 40}
+c.proxy.c = {k: {d: 15}, g: 15}
 
-const obj = {a: {b: 5, c: 6}, e: 40}
-const proxy = new Proxy(obj, {
-  get(target, prop, value) {
-    console.log('Get triggered for prop', prop);
-    if (prop === 'a') {
-      return target.e
-    }
-    return target[prop]
-  },
-  set(target,prop,value, receiver) {
-    if (prop === 'x') {
-      console.log('speial trigger, getting a for fun');
-      console.log('is a a or e?', this.get(target, 'a', receiver));
-    }
-    target[prop] = value
-    return true
-  }
-})
-proxy.x = 5
+console.log('proxy', c.proxy);
+console.log('pristine', c.pristine);
 
-window.pack = pack
+console.log('--- check merges and deletes; should be empty, because we did not propagate');
+console.log('pristine', c.pristine);
+console.log('merges', c.merges);
+console.log('deletes', c.deletes);
 
-window.con = c
-window.p = c.proxy;
-window.c = c.pristine;
-window.ch = c.merges;
-window.m = c.meta
-window.d = c.deletes
+console.log('changed handlers',c.handlersWithChanges)
+
+c.propagateChanges()
+console.log('--- after changes propagations');
+
+console.log('pristine', c.pristine);
+console.log('merges', c.merges);
+console.log('deletes', c.deletes);
+
+c.proxy.b.x = 15
+c.proxy.d = 7
+c.propagateChanges()
+console.log('--- after basic changes propagations');
+
+console.log('pristine', c.pristine);
+console.log('merges', c.merges);
+console.log('deletes', c.deletes);
+
+console.log();
+
+// delete c.proxy.a
+// delete c.proxy.b
+// c.proxy.c
+delete c.proxy.c.g
+delete c.proxy.c.k
+delete c.proxy.c
+// delete c.proxy.d
+c.propagateChanges()
+console.log('--- after some deletes');
+
+console.log('pristine', c.pristine);
+console.log('merges', c.merges);
+console.log('deletes', c.deletes);
