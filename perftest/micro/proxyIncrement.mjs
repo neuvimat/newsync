8e8468da829ce14e107d90c61229e53ab4deed81
Matyáš Neuvirt <matyas.neuvirt@gmail.com>
2022-05-15 03:28:45 +0200

    updates to tests and added README.md at places

--- perftest/micro/proxyIncrement.mjs
+++ perftest/micro/proxyIncrement.mjs
@@ -2,7 +2,7 @@ import * as t from '../lib/Library.mjs'
 
 let native, reflectProxy, basicProxy, emptyProxy, toBeProxied1, toBeProxied2, toBeProxied3;
 
-t.beforeEach(() => {
+t.before(() => {
   native = {number: 0}
   toBeProxied1 = {number: 0}
   toBeProxied2 = {number: 0}
@@ -18,6 +18,30 @@ t.beforeEach(() => {
       }
     })
   emptyProxy = new Proxy(toBeProxied3, {})
+})
+
+t.test('Native += add', () => {
+  for (let i = 0; i < 100_000; i++) {
+    native.number += Math.random()
+  }
+})
+
+t.test('Reflect proxy += add', () => {
+  for (let i = 0; i < 100_000; i++) {
+    reflectProxy.number += Math.random()
+  }
+})
+
+t.test('Basic proxy += add', () => {
+  for (let i = 0; i < 100_000; i++) {
+    basicProxy.number += Math.random()
+  }
+})
+
+t.test('Empty proxy += add', () => {
+  for (let i = 0; i < 100_000; i++) {
+    emptyProxy.number += Math.random()
+  }
 })
 
 t.test('Native add', () => {
@@ -41,6 +65,54 @@ t.test('Basic proxy add', () => {
 t.test('Empty proxy add', () => {
   for (let i = 0; i < 100_000; i++) {
     emptyProxy.number = emptyProxy.number + Math.random()
+  }
+})
+
+t.test('Native get', () => {
+  for (let i = 0; i < 100_000; i++) {
+    native.number
+  }
+})
+
+t.test('Reflect proxy get', () => {
+  for (let i = 0; i < 100_000; i++) {
+    reflectProxy.number
+  }
+})
+
+t.test('Basic proxy get', () => {
+  for (let i = 0; i < 100_000; i++) {
+    basicProxy.number
+  }
+})
+
+t.test('Empty proxy get', () => {
+  for (let i = 0; i < 100_000; i++) {
+    emptyProxy.number
+  }
+})
+
+t.test('Native set', () => {
+  for (let i = 0; i < 100_000; i++) {
+    native.number = Math.random()
+  }
+})
+
+t.test('Reflect proxy set', () => {
+  for (let i = 0; i < 100_000; i++) {
+    reflectProxy.number = Math.random()
+  }
+})
+
+t.test('Basic proxy set', () => {
+  for (let i = 0; i < 100_000; i++) {
+    basicProxy.number = Math.random()
+  }
+})
+
+t.test('Empty proxy set', () => {
+  for (let i = 0; i < 100_000; i++) {
+    emptyProxy.number = Math.random()
   }
 })
 
