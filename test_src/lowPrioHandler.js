fab18a56f25fb869b26d1c06eff6a060a027819d
Matyáš Neuvirt <matyas.neuvirt@gmail.com>
2022-05-18 20:39:58 +0200

    low prio done

--- test_src/lowPrioHandler.js
+++ test_src/lowPrioHandler.js
@@ -3,24 +3,44 @@ import {ObjectContainer} from "@Lib/shared/containers/ObjectContainer";
 import {isEmpty} from "@Lib/objUtil";
 import assert from "assert";
 import * as _ from 'lodash'
-import {SYMBOLS} from "@Lib/shared/SYMBOLS";
+import {INDICES, SYMBOLS} from "@Lib/shared/SYMBOLS";
 import {NewSyncServer} from "@Lib/server/NewSyncServer";
 import {LongKeyDictionaryServer} from "@Lib/shared/LongKeyDictionaryServer";
 import {MessagePackCoder} from "@Lib/shared/coder/MessagePackCoder";
 import {RtcDriverServer} from "@Lib/server/drivers/RtcDriverServer";
 
-let ns, c, cl
+let ns, container, client, dict
 
 beforeEach(() => {
-  ns = new NewSyncServer(new RtcDriverServer(), new MessagePackCoder(), new LongKeyDictionaryServer())
+  dict = new LongKeyDictionaryServer(3)
+  ns = new NewSyncServer(new RtcDriverServer(), new MessagePackCoder(), dict)
   ns.welcome = ()=>{} // Mock
-  c = ns.addContainer('myContainer', new ObjectContainer())
-  cl = ns.addClient()
-  cl.whitelistContainer('myContainer')
+  container = ns.addContainer('myContainer', new ObjectContainer())
+  client = ns.addClient()
+  client.whitelistContainer('myContainer')
 })
 
 describe('Basic tests:', () => {
   it('Low prio changes are in container', () => {
+    container.pristine.a = 15
 
+    assert(container.pristine.a === 15)
+    assert(isEmpty(container.merges), '`merges` is not empty!')
+    assert(isEmpty(container.lowPrio), '`lowPrio` is not empty!')
+
+    container.proxy[SYMBOLS.sLow].set('b', 20).set('a',19)
+    container.propagateChanges()
+    assert(isEmpty(container.merges), '`merges` is not empty!')
+    assert(container.pristine.a === 19, '`pristine.a` is not 19!')
+    assert(container.pristine.b === 20, '`pristine.b` is not 20!')
+    assert(container.lowPrio.a === 19, '`lowPrio.a` is not 19!')
+    assert(container.lowPrio.b === 20, '`lowPrio.b` is not 20!')
+
+    const lpMsg = ns.getLowPrioChangesMessage(client) // returns packed container under shortened key
+    const changes = lpMsg[INDICES.merges]
+    const actualChanges = ns.coder.unpack(changes[dict.shorten('myContainer')])
+    assert(_.isEqual(actualChanges, [{b:20,a:19}]))
+
+    container.clear()
   })
 })
