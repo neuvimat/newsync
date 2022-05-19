e6c0e1ba32053b4a9965fa53d14bc66a7282d21b
Matyáš Neuvirt <matyas.neuvirt@gmail.com>
2022-05-20 00:33:48 +0200

    finishing touches

--- src/fe/MainPlayground.js
+++ src/fe/MainPlayground.js
@@ -1,63 +1,19 @@
+// noinspection DuplicatedCode
 
-import * as x from 'arraydiff'
-import * as y from 'fast-array-diff'
-import * as z from 'jsondiffpatch'
+import {LongKeyDictionaryServer} from "@Lib/server/LongKeyDictionaryServer";
+import {NewSyncServer} from "@Lib/server/NewSyncServer";
+import {RtcDriverServer} from "@Lib/server/drivers/RtcDriverServer";
+import {MessagePackCoder} from "@Lib/shared/coder/MessagePackCoder";
+import {ObjectContainer} from "@Lib/shared/container/ObjectContainer";
+import {applyMeta} from "@Lib/objUtil";
 
-import * as _ from 'lodash'
-import assert from "assert";
-import {fullCompare} from "@Lib/shared/proxies/ObjectProxyHandler";
-import {byteSize} from "@Lib/format.mjs";
 
-window._ = _
-window.byteSize = byteSize
+console.log('x');
+let ns, container, client, dict
 
-const obj1 = {one: 1, two: 'two', thre: [1, 2, 3], nest: {a: 15}}
-const obj2 = {lolec: 'true', akafuka: true, nested: {arrayszs: [1, 2, 3]}, sivt: [6, 9]}
-
-const arr1 = [obj2, obj1]
-const arr2 = [obj1, obj2, obj2]
-
-console.log('x', x(arr1, arr2));
-console.log('y', y.diff(arr1, arr2));
-console.log('y', y.getPatch(arr1, arr2));
-console.log('z', z.diff(arr1, arr2))
-console.log('z2', z.patch([...arr1], z.diff(arr1,arr2)))
-
-const patch = y.getPatch(arr1,arr2)
-console.log('patch', patch);
-let arr = [...arr1]
-arr = y.applyPatch(arr, patch)
-console.log('arr', arr);
-
-const proxyE = document.getElementById('proxy');
-const simE = document.getElementById('sim');
-const changesE = document.getElementById('changes');
-const clearE = document.getElementById('clear');
-
-// const c = new ObjectContainer()
-// c.proxy.a = {array: ['a', 'b', 'c']}
-// c.propagateChanges()
-//
-// console.log('============= juicy stuff');
-//
-// c.proxy.a.array = c.proxy.a.array.filter((e)=>{return e === 'b'})
-//
-// console.log(c.proxy.a.array);
-//
-// console.log('PUSH');
-// c.proxy.a.array.push(5)
-//
-// console.log(c.proxy.a.array);
-
-const a = {lol: 15, wth: 'what the hell', nest: {egg: 2, species: 'ostrich'}}
-const b = {one: 1, two: 'two', three: {x: 15, c: 25}}
-
-const array1 = [a, b, 5, 6, 7, 8, 9]
-const array2 = [b, b, 5, 6, 11, 7, 9]
-
-// const c = new ObjectContainer()
-// c.proxy.a = array1
-// c.proxy.b = array2
-
-const result = fullCompare(array2, array1)
-console.log('result', result);
+dict = new LongKeyDictionaryServer(3)
+ns = new NewSyncServer(new RtcDriverServer(), new MessagePackCoder(), dict)
+ns.welcome = () => {} // Mock
+container = ns.addContainer('myContainer', new ObjectContainer())
+client = ns.addClient()
+client.whitelistContainer('myContainer')
---
+++ test_src/arrayHandler.js
@@ -0,0 +1,49 @@
+// import * as assert from "assert";
+import {ObjectContainer} from "@Lib/shared/container/ObjectContainer";
+import {applyMeta, isEmpty} from "@Lib/objUtil";
+import assert from "assert";
+import * as _ from 'lodash'
+import {INDICES, SYMBOLS} from "@Lib/shared/SYMBOLS";
+import {NewSyncServer} from "@Lib/server/NewSyncServer";
+import {LongKeyDictionaryServer} from "@Lib/server/LongKeyDictionaryServer";
+import {MessagePackCoder} from "@Lib/shared/coder/MessagePackCoder";
+import {RtcDriverServer} from "@Lib/server/drivers/RtcDriverServer";
+
+let ns, container, client, dict
+
+beforeEach(() => {
+  dict = new LongKeyDictionaryServer(3)
+  ns = new NewSyncServer(new RtcDriverServer(), new MessagePackCoder(), dict)
+  ns.welcome = ()=>{} // Mock
+  container = ns.addContainer('myContainer', new ObjectContainer())
+  client = ns.addClient()
+  client.whitelistContainer('myContainer')
+})
+
+describe('Basic tests:', () => {
+  it('Basic Array proxy functionality', () => {
+    const objA = {one: 'one', two: 'two'}
+    const objB = {three: 'three', four: 'four'}
+    container.proxy.a = [objA, objB, objA]
+    container.propagateChanges()
+    container.clear()
+
+    container.proxy.a.splice(0,1)
+    container.propagateChanges()
+    assert(container.pristine.a.length === 2, 'Pristine was not correctly updated!')
+    let arr = [objA, objB, objA]
+    let changes = container.meta
+    assert(!isEmpty(changes), 'There are no meta changes!')
+    applyMeta({a: arr}, changes) // Create a fake array that imitates the container's one before splice and apply the chagnes
+    assert(_.isEqual(arr, container.pristine.a), 'Applying meta changes did not result to the proper result!')
+    container.clear()
+
+    container.proxy.a.push({xd: 15}, objA)
+    container.propagateChanges()
+    assert(!isEmpty(changes), 'There are no meta changes!')
+    arr = [objB, objA]
+    changes = container.meta
+    applyMeta({a: arr}, changes) // Create a fake array that imitates the container's one before splice and apply the chagnes
+    assert(_.isEqual(arr, container.pristine.a), 'Applying meta changes did not result to the proper result!')
+  })
+})
