331ef31d737fb882fafbcb959fa5eab7be8c8b81
Matyáš Neuvirt <matyas.neuvirt@gmail.com>
2022-09-19 01:56:12 +0200

    made public path configurable in .env

--- vue.config.js
+++ vue.config.js
@@ -11,6 +11,8 @@ module.exports = {
       .set("@", path.join(__dirname, "./src"))
       .set("@Lib", path.join(__dirname, "./lib"))
       .set("@FE", path.join(__dirname, "./src/fe"))
-      .set('@BE', path.join(__dirname, "./src/be"));
+      .set('@BE', path.join(__dirname, "./src/be"))
+      .end();
+    config.output.publicPath(process.env.VUE_APP_PUBLIC_PATH)
   }
 };
